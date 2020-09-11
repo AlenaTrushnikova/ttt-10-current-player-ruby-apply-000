cc95c8620c7a3417ef2377c0e1b374622fc06901
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-11 22:57:02 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,5 +8,10 @@ def turn_count(board)
 end
 
 def current_player(board)
-   turn_count(board) % 2 == 0 ? "X" : "O"
+  turns = turn_count(board)
+  if turns % 2 == 0
+    return "O"
+  else
+    return "X"
+  end
 end
